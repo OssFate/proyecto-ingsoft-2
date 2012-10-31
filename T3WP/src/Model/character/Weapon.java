d523474760e5d097a8852eee44498a79d4a8d767
Alex <Alex@Molinas-PC.lan>
2012-10-30 21:54:48 -0500

    Added the inherited classes form the class Weapon

--- T3WP/src/Model/character/Weapon.java
+++ T3WP/src/Model/character/Weapon.java
@@ -3,7 +3,6 @@
  * and open the template in the editor.
  */
 package Model.character;
-
 /**
  *
  * @author usuarios
@@ -11,9 +10,11 @@ package Model.character;
 public class Weapon implements Attackable{
     private int normalDamage;
     private int specialDamage;
+    private int range;
     
     public Weapon(){}
 
     public int getNormalDamage() { return normalDamage; }
     public int getSpecialDamage() { return specialDamage; }
+    public int getRange() { return range; }
 }
---
+++ T3WP/src/Model/weapons/HugeMace.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.weapons;
+import Model.character.Weapon;
+
+/**
+ *
+ * @author Alex
+ */
+public class HugeMace extends Weapon{
+    public HugeMace() { }
+}
